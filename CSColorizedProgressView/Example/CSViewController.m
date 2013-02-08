fef25c861f7c3b0d259d7bda8e3c3432533468ed
Charles Scalesse <cscalesse@jetsetter.com>
2013-02-08 11:28:34 -0500

    Example allows for multiple directions

--- CSColorizedProgressView/Example/CSViewController.m
+++ CSColorizedProgressView/Example/CSViewController.m
@@ -13,6 +13,7 @@
 
 - (IBAction)handleAnimateTapped;
 - (IBAction)handleResetTapped;
+- (IBAction)handleDirectionTapped:(UIButton *)directionButton;
 
 @end
 
@@ -35,12 +36,22 @@
 #pragma mark - Events
 
 - (IBAction)handleAnimateTapped {
+    [self handleResetTapped];
     [_colorizedProgressView setProgress:1.0 animated:YES];
 }
 
 - (IBAction)handleResetTapped {
     [_colorizedProgressView stopAnimation];
     _colorizedProgressView.progress = 0.0;
+}
+
+- (IBAction)handleDirectionTapped:(UIButton *)directionButton {
+    switch (directionButton.tag) {
+        case 0: _colorizedProgressView.direction = CSColorizedProgressViewDirectionBottomToTop; break;
+        case 1: _colorizedProgressView.direction = CSColorizedProgressViewDirectionTopToBottom; break;
+        case 2: _colorizedProgressView.direction = CSColorizedProgressViewDirectionLeftToRight; break;
+        case 3: _colorizedProgressView.direction = CSColorizedProgressViewDirectionRightToLeft; break;
+    }
 }
 
 @end
