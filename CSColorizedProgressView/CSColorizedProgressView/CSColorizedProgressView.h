d4c7c6234c483a84f110cabdf66d143e517b9916
Charles Scalesse <scalessec@gmail.com>
2013-02-09 19:25:39 -0500

    Fixing license formatting

--- CSColorizedProgressView/CSColorizedProgressView/CSColorizedProgressView.h
+++ CSColorizedProgressView/CSColorizedProgressView/CSColorizedProgressView.h
@@ -6,12 +6,11 @@ CSColorizedProgressView
 Copyright (c) 2013 Charles Scalesse.
 
 Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the
-                                                          "Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software 
+is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included
 in all copies or substantial portions of the Software.
